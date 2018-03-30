90e1f7ada9cb0d6a8ad482a9015c33318d84e287
Bobby Martin <robert.martin.619@gmail.com>
2018-03-30 11:16:39 -0700

    Rearrange conditional so that teens are divisible by 3 first

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@ function beepBoop(number) {
   for (var i = 0; i <= number; i++) {
     var strArray = i.toString().split('');
     console.log(strArray);
-    if (i === 0) {
-      numArray.push("Beep!");
-    } else if (i === 1) {
-      numArray.push("Boop!");
-    } else if (i % 3 === 0) {
+    if (i % 3 === 0) {
       numArray.push("I'm sorry Dave. I'm afraid I can't do that.");
+    } else if (parseInt(strArray) === 1) {
+      numArray.push("Boop!");
+    } else if (parseInt(strArray) === 0) {
+      numArray.push("Beep!");
     } else {
       numArray.push(i);
     }
