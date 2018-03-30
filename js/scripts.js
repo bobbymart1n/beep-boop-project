1abe62c4083266af9f4b1e86ef944e1ef1e00d71
Bobby Martin <robert.martin.619@gmail.com>
2018-03-30 10:09:02 -0700

    Add third spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 function beepBoop(number) {
   if (number === 0) {
-    return "Beep!"
+    return "Beep!";
   } else if (number === 1) {
-    return "Boop!"
+    return "Boop!";
+  } else if (number % 3 === 0) {
+    return "I'm sorry Dave. I'm afraid I can't do that.";
   }
 }
