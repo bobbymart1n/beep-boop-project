228705852465467d36a38ccda60160149503a7fa
Bobby Martin <robert.martin.619@gmail.com>
2018-03-30 11:54:53 -0700

    Add parseInt to look for related numbers, still need to account for 20+

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function beepBoop(number) {
   var numArray = [];
   for (var i = 0; i <= number; i++) {
     var strArray = i.toString().split('');
-    console.log(strArray);
+    // console.log(strArray);
     if (i % 3 === 0) {
       numArray.push("I'm sorry Dave. I'm afraid I can't do that.");
     } else if (parseInt(strArray) === 1) {
