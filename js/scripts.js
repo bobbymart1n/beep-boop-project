cf4410f708a6456f8891aafce8dd89b5f22c1715
Bobby Martin <robert.martin.619@gmail.com>
2018-03-30 13:14:29 -0700

    Add .includes to look for 1s or 0s rather than hard value

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@ function beepBoop(number) {
   var numArray = [];
   for (var i = 0; i <= number; i++) {
     var strArray = i.toString().split('');
-    // console.log(strArray);
+    console.log(strArray);
     if (i % 3 === 0) {
       numArray.push("I'm sorry Dave. I'm afraid I can't do that.");
-    } else if (parseInt(strArray) === 1) {
+    } else if(strArray.includes("1")) {
       numArray.push("Boop!");
-    } else if (parseInt(strArray) === 0) {
+    } else if (strArray.includes("0")) {
       numArray.push("Beep!");
     } else {
       numArray.push(i);
