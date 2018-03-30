c361421ac8e08d7b149aca043989fc494fcb9f0a
Bobby Martin <robert.martin.619@gmail.com>
2018-03-30 11:02:56 -0700

    Add new conditional to push other numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 function beepBoop(number) {
-  if (number === 0) {
-    return "Beep!";
-  } else if (number === 1) {
-    return "Boop!";
-  } else if (number % 3 === 0) {
-    return "I'm sorry Dave. I'm afraid I can't do that.";
+  var numArray = [];
+  for (var i = 0; i <= number; i++) {
+    console.log(number[i]);
+    if (i === 0) {
+      numArray.push("Beep!");
+    } else if (i === 1) {
+      numArray.push("Boop!");
+    } else if (i % 3 === 0) {
+      numArray.push("I'm sorry Dave. I'm afraid I can't do that.");
+    } else {
+      numArray.push(i);
+    }
   }
+  console.log(numArray);
 }
